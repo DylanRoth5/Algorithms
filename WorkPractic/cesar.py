2827b2d1a8023183aa0dd91aca54bb162ec17305
DylanRoth5 <130003124+DylanRoth5@users.noreply.github.com>
2023-05-11 16:09:35 -0300

    a

--- WorkPractic/cesar.py
+++ WorkPractic/cesar.py
@@ -1,2 +1,16 @@
+# Objetivo:
+# 1) Generar un algoritmo que permita el ingreso de una frase y nro de desplazamiento x y le aplique el cifrado César.
+# 2) Algoritmo del camino inverso al punto 1
+# 3) Puntos extras: un algoritmo que con solo ingresar la frase cifrada logre encontrar el nro de desplazamiento que se usó y la descifre.
+
+# Reglas de cifrado:
+# 1) Usar el método de cifrado César
+# 2) Usar palabras del diccionario en español
+# 3) Se puede desplazar arbitrariamente el alfabeto
+# 4) Separar espacios entre palabras
+# 5) No codificar los espacios
+# 6) Se sugiere usar una frase de algún famoso inspiradora
+# 7)“Soy optimista, no veo que sea útil ser otra cosa” WChurchill
+
 def cifrado_cesar():
     print('cesar OMG')
