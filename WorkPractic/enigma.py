5ead071f02d737528052b0fccf040ebe903e6092
DylanRoth5 <130003124+DylanRoth5@users.noreply.github.com>
2023-05-28 23:16:39 -0300

    Update enigma.py

--- WorkPractic/enigma.py
+++ WorkPractic/enigma.py
@@ -53,3 +53,108 @@ class Position:
                 return self.abecedario[position]
 
 
+class Rotor_V:
+    position = None
+    entry = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    out = ["V", "Z", "B", "R", "G", "I", "T", "Y", "U", "P", "S", "D", "N", "H", "L", "X", "A", "W", "M", "J", "Q", "O", "F", "E", "C", "K"]
+        
+    def __init__(self, initial_pos="A"):
+        self.position = Position(initial_pos)
+
+    def code(self, letter):
+        letter = self.position.real_in(letter)
+        for pos, l in enumerate(self.entry):
+            if letter == l:
+                letter = self.out[pos]
+                break
+        return letter
+    
+    def decode(self, letter):        
+        for pos, l in enumerate(self.out):
+            if letter == l:
+                letter = self.entry[pos]
+                break
+        letter = self.position.real_out(letter)
+        return letter
+
+class Rotor_IV:
+    position = None
+    entry = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    out = ['E','S','O','V','P','Z','J','A','Y','Q','U','I','R','H','X','L','N','F','T','G','K','D','C','M','W','B']
+        
+    def __init__(self, initial_pos="A"):
+        self.position = Position(initial_pos)
+
+    def code(self, letter):
+        letter = self.position.real_in(letter)
+        for pos, l in enumerate(self.entry):
+            if letter == l:
+                letter = self.out[pos]
+                break
+        return letter
+    
+    def decode(self, letter):        
+        for pos, l in enumerate(self.out):
+            if letter == l:
+                letter = self.entry[pos]
+                break
+        letter = self.position.real_out(letter)
+        return letter
+    
+class Rotor_I:
+    position = None
+    entry = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    out = ["E","K","M","F","L","G","D","Q","V","Z","N","T","O","W","Y","H","X","U","S","P","A","I","B","R","C","J"]
+        
+    def __init__(self, initial_pos="A"):
+        self.position = Position(initial_pos)
+
+    def code(self, letter):
+        letter = self.position.real_in(letter)
+        for pos, l in enumerate(self.entry):
+            if letter == l:
+                letter = self.out[pos]
+                break
+        return letter
+    
+    def decode(self, letter):        
+        for pos, l in enumerate(self.out):
+            if letter == l:
+                letter = self.entry[pos]
+                break
+        letter = self.position.real_out(letter)
+        return letter
+
+
+rotorV  = Rotor_V("B")
+rotorIV = Rotor_IV("A")
+rotorI  = Rotor_I("C")
+
+frase = input("Ingrese una frase: ").upper()
+
+code = ""
+rotations = 0
+for letra in frase:
+    if letra != " ":
+        rotorV.position.rotate()
+        rotations+=1
+        code += rotorV.code(letra)
+    else:
+        code += " "
+
+
+
+
+print("Code: " + code)
+
+rotorV.position.reset()
+
+decoded = ""
+for letra in code:
+    if letra != " ":
+        rotorV.position.rotate()
+        decoded += rotorV.decode(letra)
+    else:
+        decoded += " "
+        
+print("Decoded: " + decoded)
