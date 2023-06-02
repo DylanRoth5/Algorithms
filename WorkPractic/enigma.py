484c2665fb52d451501028203b258cd2c6d96215
DylanRoth5 <130003124+DylanRoth5@users.noreply.github.com>
2023-06-02 16:31:33 -0300

    Update enigma.py

--- WorkPractic/enigma.py
+++ WorkPractic/enigma.py
@@ -196,7 +196,7 @@ phase_two = ''
 for item in phase_one:
     for letter in abecedario:
         if letter == item:
-            # print(phase_one.index(letter)+1)
+            # print(phase_one.index(letter)+1) 
             # print(letter,abecedario.index(letter)-(phase_one.index(letter)+1))
 
             # print(rotorIV[abecedario.index(letter)-(phase_one.index(letter)+1)])
